40e8f7e365b9fc51dd758cdd94253cf24eaaf19d
guest256 <79742888+guest256@users.noreply.github.com>
2023-06-22 19:50:55 +0200

    enemies can die :)

--- enemy.cs
+++ enemy.cs
@@ -9,6 +9,12 @@ public partial class enemy : CharacterBody2D
 	Vector2 speed = new Vector2(120,120);
 	Vector2 spawnradius = new Vector2(1000,1000);
 	
+	private void _on_area_2d_area_entered(Area2D area)
+	{
+		this.QueueFree();
+	}
+	
+	
 	public override void _Ready(){
 		float rngnumber = Convert.ToSingle(GD.RandRange(-3.14, 3.14));
 		
@@ -23,3 +29,6 @@ public partial class enemy : CharacterBody2D
 		MoveAndSlide();
 	}
 }
+
+
+
