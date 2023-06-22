a56ece0ff8ce00396fdb11cc48e1f2826ebd8462
guest256 <79742888+guest256@users.noreply.github.com>
2023-06-22 18:24:51 +0200

    better swordsystem

--- sword.cs
+++ sword.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class sword : Sprite2D
 {
-	
 	dynamic parent;
+	float angletomouse = 0f;
+	
+	Vector2 velocity = new Vector2(0,0);
+	Vector2 sword_radius = new Vector2(20, 25);
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,6 +24,14 @@ public partial class sword : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		parent.LookAt(GetGlobalMousePosition());
+		angletomouse = parent.GetAngleTo(GetGlobalMousePosition());
+		
+		this.Position = new Vector2(Mathf.Cos(angletomouse), Mathf.Sin(angletomouse)) * sword_radius;
+		
+		
+		this.LookAt(GetGlobalMousePosition());
+		
+		
+		
 	}
 }
