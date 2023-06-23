a83a9e42955b6a3bb2f8d2b555952509ee4887e0
guest256 <79742888+guest256@users.noreply.github.com>
2023-06-23 14:01:48 +0200

    move spawnradius

--- enemy.cs
+++ enemy.cs
@@ -8,6 +8,7 @@ public partial class enemy : CharacterBody2D
 	
 	Vector2 speed = new Vector2(120,120);
 	Vector2 spawnradius = new Vector2(1000,1000);
+	Vector2 move_spawnradius = new Vector2(500,250);
 	
 	private void _on_area_2d_area_entered(Area2D area)
 	{
@@ -18,7 +19,7 @@ public partial class enemy : CharacterBody2D
 	public override void _Ready(){
 		float rngnumber = Convert.ToSingle(GD.RandRange(-3.14, 3.14));
 		
-		this.Position = new Vector2(Mathf.Cos(rngnumber), Mathf.Sin(rngnumber)) * spawnradius;
+		this.Position = new Vector2(Mathf.Cos(rngnumber), Mathf.Sin(rngnumber)) * spawnradius + move_spawnradius;
 		
 		target = GetParent().GetNode<Godot.CharacterBody2D>("Player");
 	}
